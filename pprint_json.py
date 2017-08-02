f54f64f41bde45a284f78e0c10568ccdd594e4fd
Valery Ramusik <valer2677.@gmailcom>
2017-08-02 03:53:52 +0300

    prettyprint json from filepath

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        parsed = json.load(file)
+    return parsed
 
 
 def pretty_print_json(data):
-    pass
+    pretty_jsn = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
+    print(pretty_jsn)
 
 
 if __name__ == '__main__':
-    pass
+    jsn_file = sys.argv[1]
+    jsn_data = load_data(jsn_file)
+    pretty_print_json(jsn_data)
