f3a3bbc8c87d6f2ae4467f19e52b1bb60a28dda5
Valery Ramusik <valer2677.@gmailcom>
2017-08-06 05:46:25 +0300

    Add sys.exit() in check len(sys.argv)

--- pprint_json.py
+++ pprint_json.py
@@ -23,8 +23,10 @@ def pretty_print_json(jsn_data):
 if __name__ == '__main__':
     if not len(sys.argv) > 1:
         print('\nEnter: python3 pprint_json.py "filepath"\n')
+        sys.exit()
     jsn_filepath = sys.argv[1]
     jsn_data = load_data(jsn_filepath)
     if not jsn_data:
         print('filepath does not exist')
-    pretty_print_json(jsn_data)
+    else:
+        pretty_print_json(jsn_data)
